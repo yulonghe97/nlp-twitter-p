fb408f14c6093882fff969c0dbee0015a840dfd6
YULONG HE <charlesyulong@yahoo.com>
2020-05-11 23:59:47 -0400

    updated config

--- src/algo/db.js
+++ src/algo/db.js
@@ -8,7 +8,7 @@ const clue = new mongoose.Schema({
 const wordClue = mongoose.model('wordClue', clue);
 
 // DATABASE
-const USERNAME = 'nlp';
-const PASSWORD = '110110';
+const USERNAME = '';
+const PASSWORD = '';
 mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@rmc0-quigt.mongodb.net/nlp?retryWrites=true&w=majority`,{ useUnifiedTopology: true, useNewUrlParser: true});
 module.exports = wordClue;
